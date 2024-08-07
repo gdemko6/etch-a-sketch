145d18562b817a2a6b6815f650fc25bd2d81e9e2
Greg Demko <gregory.demko@my.uwrf.edu>
2024-08-07 17:59:39 -0500

    Add hover function so that users can draw by hovering their mouse over the boxes

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -7,16 +7,21 @@ for (let i = 0; i<16; i++){
     const row = document.createElement("div");
     row.classList.add("row");
     rows.push(row);
-    
 }
-    //put 16 divs inside of each of the rows
-    rows.forEach((row) =>{
-        container.appendChild(row);
-        for (let i=0; i<16; i++){
-            const box = document.createElement("div");
-            box.classList.add("box");
-            row.appendChild(box);
-        }
-    })
 
+//put 16 divs inside of each of the rows
+rows.forEach((row) =>{
+    container.appendChild(row);
+    for (let i=0; i<16; i++){
+        const box = document.createElement("div");
+        box.classList.add("box");
+        row.appendChild(box);
+        box.addEventListener("mouseover", changeColor);
+    }
+});
+
+function changeColor(){
+   this.style.border = "1px solid red";
+   this.style.backgroundColor = "black";
+}
 
