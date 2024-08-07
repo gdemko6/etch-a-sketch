965fe537669de1fc59bf788a0c2655d7486148ac
Greg Demko <gregory.demko@my.uwrf.edu>
2024-08-06 21:52:12 -0500

    Add 16*16 boxes for etch a sketch

---
+++ etch-a-sketch.js
@@ -0,0 +1,22 @@
+const container = document.querySelector(".container");
+
+const rows = [];
+
+//create 16 divs that will be containers for the rows
+for (let i = 0; i<16; i++){
+    const row = document.createElement("div");
+    row.classList.add("row");
+    rows.push(row);
+    
+}
+    //put 16 divs inside of each of the rows
+    rows.forEach((row) =>{
+        container.appendChild(row);
+        for (let i=0; i<16; i++){
+            const box = document.createElement("div");
+            box.classList.add("box");
+            row.appendChild(box);
+        }
+    })
+
+
